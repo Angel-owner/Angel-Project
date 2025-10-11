8381accd36cc43cd9ea5793753a829ad5c95ad0b
razigli <razigli@pc.dev>
2025-10-11 18:53:23 +0500

    сделал игру 'угадай число'

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-a = int(input())
-print(a ** 2)
+import random
+
+number = random.randint(1, 100)
+
+user_number = 0
+
+i = 0
+
+while user_number != number:
+    user_number = int(input("Введиите число от 1 до 100: "))
+    if user_number < number:
+        print("Ваше число меньше, чем искомое")
+    elif user_number > number:
+        print("Ваше число больше, чем искомое")
+    i += 1
+
+print(f"Ты угада число, за число попыток: {i}")
